2dbfae93bb7a3ca9b75bf86ba70d165015b80f04
Ibrahim Mohamed <ibrahim.mohamed4@vodafone.com>
2024-02-22 20:29:34 +0200

    finalize

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,22 @@ import Navbar from './Navbar';
 import Hero from './Hero';
 import CustomButton from './CustomButton';
 import Footer from './Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CarDetails from './CarDetails';
+import ShowMore from './ShowMore';
 
-export { Navbar, Hero, CustomButton, Footer };
+export {
+  Navbar,
+  Hero,
+  CustomButton,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+};
